4d0e4d8b17458bc7c5894e43f658c7a968d36622
Sachin Yadav <64295890+sachinyadav22@users.noreply.github.com>
2022-11-23 22:20:39 +0530

    Add files via upload

---
+++ Atm_class.py
@@ -0,0 +1,62 @@
+class Atm :
+    def __init__(self):
+        self.__pin=""
+        self.__balance= 0
+
+        self.menu()
+
+    def menu(self):
+        user_input = input('''
+                                Hello, how whould you like to proceed?
+                                1. Enter 1 to create a pin.
+                                2. Enter 2 to deposit.
+                                3. Enter 3 to withdraw.
+                                4. Enter 4 to check balance.
+                                5. Enter 5 to Exit.
+        ''')
+        if user_input == '1':
+            self.create_pin()
+        elif user_input == '2':
+            self.deposit()
+        elif user_input == '3':
+            self.withdraw()
+        elif user_input == '4':
+            self.check_balance()
+        else:
+            print("Thanks for visiting.")
+
+    def create_pin(self):
+        self.__pin = input("Enter pin")
+        print("Pin set successfully")
+        self.menu()
+    
+    def deposit(self):
+        temp = input("Enter your pin.")
+        if temp == self.__pin:
+            amount = int(input("Enter the amount."))
+            self.__balance = self.__balance + amount
+            print("Deposit Successfully Done")
+        else:
+            print("Invalid Pin")
+        self.menu()
+
+    def withdraw(self):
+        temp = input("Enter your pin.")
+        if temp == self.__pin:
+            amount = int(input("Enter the amount."))
+            if amount < self.__balance:
+                self.__balance = self.__balance - amount
+                print("Operation successfully done.")
+            else:
+                print("insufficient Funds in acount.")
+        else:
+            print("Invalid Pin")
+        self.menu()
+    
+    def check_balance(self):
+        temp = input("Enter your pin.")
+        if temp == self.__pin:
+            print(self.__balance)
+        else:
+            print("Inavalid pin")
+        self.menu()
